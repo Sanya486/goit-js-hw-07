7eeea825e423717f0baf88188309f7453cfde5a4
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-04-23 21:44:50 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+};
+
+
+
+function galleryMarkup (galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href=""${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    }).join('');
+};
+
+refs.galleryEl.innerHTML = galleryMarkup (galleryItems);
+
+
+refs.galleryEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  else {
+
+
+    function modalCloseOnEscape(e) {
+      if (e.code === "Escape") {
+        instance.close();
+        console.log(e.key);
+      };
+    };
+
+    const instance = basicLightbox.create(`<img
+            src="${e.target.dataset.source}"
+            alt="${e.target.alt}"/>`, {
+      onClose: () => {
+              document.removeEventListener('keydown', modalCloseOnEscape)
+      },
+      onShow: () => {
+              document.addEventListener('keydown', modalCloseOnEscape)
+    }});
+    instance.show(); 
+
+  };
+});
+
+
+
+
